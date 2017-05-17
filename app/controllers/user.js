213193e6e472602f7f17772d6e1a200b74711094
Alexander Ljungberg Perme <alex.perme@gmail.com>
2017-05-17 12:15:54 +0200

    Continued with user functionality

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -3,4 +3,8 @@ module.exports = class User {
 
     }
 
+    loggin (cridentials) {
+        
+    }
+
 }
--- features/step_definitions/user.js
+++ features/step_definitions/user.js
@@ -1,20 +1,67 @@
 const { defineSupportCode } = require('cucumber');
 const assert = require('assert');
 
+// Require the User Class to test.
+const User = require('../../app/controllers/user.js');
+
 defineSupportCode ( ( { Given, When, Then } ) => {
-    Given('...', function (callback) {
-        //  Code for testing goes here.
-        callback(null, "pending");
+    //  Variable to ceep the user instance in.
+    let user, cookie;
+
+    Given('I am not logged in', function (callback) {
+        //  Create a new instance of a user that is not logged in.
+        user = new User();
+        // End action.
+        callback();
     });
 
-    When('...', function (callback) {
-        //  Code for testing goes here.
-        callback(null, "pending");
+    Given('I am logged in', function (callback) {
+        //  Create a new instance of a user.
+        user = new User();
+        //  Then loggin.
+        user.loggin({username: 'testUser', password: 'testPass123'});
+        // End action.
+        callback();
     });
 
-    Then('...', function (callback) {
-        //  Code for testing goes here.
-        callback(null, "pending");
+    When('I make a request with a cookie', function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+
+
+        // End action.
+        callback();
+    });
+
+    When('I make a request', function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+
+
+        // End action.
+        callback();
+    });
+
+    Then('the user class should throw an error', function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+
+
+        // End action.
+        callback();
+    });
+
+    Then('the user class should handle that cookie', function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+
+
+        // End action.
+        callback();
+    });
+
+    Then('the user class should handle that request', function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+
+
+        // End action.
+        callback();
     });
 
 });
