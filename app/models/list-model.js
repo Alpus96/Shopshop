39993c0b822dbc9b83b83902af179fdcbb10dfa4
Alexander Ljungberg Perme <alex.perme@gmail.com>
2017-05-18 13:21:40 +0200

    Added model for list table

---
+++ app/models/list-model.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const path = require('path');
+const ListTable = path.join(__dirname, 'database/ListTable.json');
+console.log(ListTable);
+
+class ListModel {
+    constructor() {}
+
+    getUsersLists (userId, callback) {
+        const table = fs.existsSync(ListTable) ? fs.readFileSync(ListTable, 'utf8') : null;
+        const existingLists = table.index ? table.index[userId].split(', ') : [];
+        let usersLists = [];
+        for (let i = 0; i < existingLists.length; i++) {
+            usersLists.push(table.lists[existingLists[i]]);
+        }
+        return usersLists;
+
+        // fs.readFileSync(ListTable, 'utf8', (err, data) => {
+        //     if (!err) {
+        //         const table = JSON.parse(data);
+        //         const existingLists = table.index[userId].split(', ');
+        //         let usersLists = [];
+        //         for (let i = 0; i < existingLists.length; i++) {
+        //             usersLists.push(table.lists[existingLists[i]]);
+        //         }
+        //         callback(null, usersLists);
+        //     } else {
+        //         console.log(err);
+        //         callback(e, null);
+        //     }
+        // });
+    }
+
+    saveList (userId, list) {
+        let found = false;
+        fs.readFile(ListTable, 'utf8', (err, data) => {
+            if (!err) {
+                table = JSON.parse(data);
+                usersLists = table.index[userId];
+                const existingLists = usersLists.split(', ');
+                for (let i = 0; i < existingLIsts.length; i++) {
+                    const listToCheck = table.lists[existingLists[i]];
+                    if (listToCheck.name === list.name) {
+                        table.lists[existingLists[i]] = list;
+                        found = true;
+                    }
+                }
+                if (!found) {
+                    table.lists.push(list);
+                    table.index[userId] += ', ' + table.lists.indexOf(list);
+                }
+                const json = JSON.stringify(table);
+                fs.writeFile(ListTable, json, 'utf8', 4);
+            } else {
+                console.error(err);
+            }
+        });
+    }
+}
+
+module.exports = new ListModel();
