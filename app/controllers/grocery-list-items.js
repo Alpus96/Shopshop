edf9a95cf3a6851c0e46acbd4fe1e2a9a9d53ce7
Alexander Ljungberg Perme <alex.perme@gmail.com>
2017-05-22 10:52:39 +0200

    Rewrote GroceryItem Class

--- app/controllers/grocery-list-items.js
+++ app/controllers/grocery-list-items.js
@@ -1,20 +1,20 @@
-module.exports = class GroceryListItem {
+class GroceryItem {
+    constructor(name, quantity, category){
+        //  !str.replace(/\s/g, '').length : confirm string length != 0, after removing all white space.
+        if (typeof name !== "string" || !name.replace(/\s/g, '').length) {
+            throw new Error("A list item must have a name that is an non-empty string.");
+        } else if (typeof quantity !== 'number' || quantity < 0) {
+            throw new Error("A quantity must be a number");
+        } else if (typeof category !== 'string' || !category.replace(/\s/g, '').length) {
+            throw new Error("There must be a category selected");
+        }
 
-  constructor(name, quantity, category){
-    if(typeof name !== "string" || name === ""){
-      throw new Error("A list item must have a name that is an non-empty string.");
+        this.name = name;
+        this.bought = false;
+        this.quantity = quantity;
+        this.category = category;
     }
-    if( quantity === " " || quantity !== parseInt(quantity, 10)){ 
-      throw new Error("A quantity must be a number");
-    }
-    if( typeof category === 'undefined'){ 
-      throw new Error("There must be a category selected");
-    }
-  
-    this.name = name;
-    this.bought = false;
-    this.quantity = quantity;
-    this.category = category;
-  }
 
 }
+
+typeof module !== 'undefined' && module.exports && typeof window === 'undefined' && (module.exports = GroceryItem);
