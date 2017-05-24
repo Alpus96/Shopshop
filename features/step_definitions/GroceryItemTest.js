69c58fc2d6a3b6fb9bb84c728973979e6fee8e73
DESKTOP-OKDL9BT\bhara <bharathiharsha>
2017-05-24 11:47:26 +0200

    Conflict

--- features/step_definitions/GroceryItemTest.js
+++ features/step_definitions/GroceryItemTest.js
@@ -6,30 +6,23 @@ defineSupportCode(function({Given, When, Then}) {
 
 	let theItem;
 
-	Given('I create a new grocery item as {name} and quantity {quantity}', function (name, quantity, callback) {
-		// console.log("this is the first line. ....");
-		// console.log(name);
-		// console.log(quantity);
-		// console.log('Now creating the object of the ')
-		 theItem = new GroceryItem(name, Number(quantity), 'baking');
-		callback();
-	});
-
-		Then('the item name should be {name}', function (name, callback) {
-			// console.log('name expected is : ' + name);
-			// console.log('name actual found is : ' + theItem.getItemName() );
-			assert(
-	           theItem.getItemName() === name,
-	           'After creating a new object of the GroceryItem, I get the name.'
-	        );
-         callback();
-       	});
-
-       	 Then('the quantity should be {int}', function (int, callback) {
-         // console.log('quantity expected is : ' + int);
-         // console.log('quantity found is : ' +  theItem.getQuantity());
-          theItem.getQuantity() === int;
+	
+	Given('I create a new grocery item as {name} and quantity as {int}', function (name,int, callback) {
+         theItem = new GroceryItem(name,int, 'baking');
          callback();
        });
 
+
+		Then('the item name should be {name}', function (name, callback) {
+			
+			assert(theItem.name === name);
+         callback();
+       	});
+
+       	 
+       
+       Then('the quantity should be {int}.', function (int, callback) {
+         assert(theItem.quantity === int);
+         callback();
+       });
 });
