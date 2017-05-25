1c6c04b3264982b3e048e400c7eedd839a5e6a3f
Alexander Ljungberg Perme <alex.perme@gmail.com>
2017-05-25 22:07:41 +0200

    Error handling added to get requests in the ajax class

--- app/views/assets/js/ajax.js
+++ app/views/assets/js/ajax.js
@@ -1,16 +1,18 @@
+/*
+*       TODO: Add error hanling for GET requets.
+* */
 class Ajax {
-    constructor() {
-
-    }
-
-    /*
-    *   A function to send ajax requests with json data to the server.
+    /**
+    *   @description    A function to send ajax requests with json data to the server.
     *
-    *   @params     url: '/example', a string containing the url where to send the post request.
-    *                         data: {username: "", password: ""}, an object containing
-    *                         the information to send to the server.
-    *                         callback: (error, result) => {...}, a function to call when done.
-    * */
+    *   @param               url:                '/example', a string containing the url where to send
+    *                                                      the post request.
+    *                                 data:            {username: "", password: ""}, an object containing
+    *                                                       the information to send to the server.
+    *                                 callback:     (error, result) => {...}, a function to call when done.
+    *
+    *   TODO: Use XMLHttpRequest here too. ($.ajax() requires jQuery)
+    **/
     post (url, data, callback) {
         //  Ajax json post request.
         $.ajax({
@@ -20,6 +22,7 @@ class Ajax {
             dataType: "json",
             success: function (result) {
                 //  Returns the result as result through the callback function.
+                //  TODO: Add comments about what response could be.
                 callback(null, result);
             },
             error: function (xhr, ajaxOptions, thrownError) {
@@ -33,17 +36,26 @@ class Ajax {
         });
     }
 
-    /*
+    /**
+    *       @description        A function to send get requests to the server.
     *
-    * */
+    *       @param                url:              '/example', a string containing the relative url for
+    *                                                           where to send the requets.
+    *                                       callback:   (error, response) => {}, a function hadling the
+    *                                                           response or error from the request.
+    **/
     get (url, callback) {
         const request = new XMLHttpRequest();
         request.open('GET', url);
         request.responseType = 'json';
         request.send();
-        request.onload = function () {
-            callback(request.response);
-        }
+        request.onload = () => {
+            callback(false, request.response);
+        };
+        request.onerror = () => {
+            console.log('Get request to ' + url + 'failed.');
+            callback(true, null);
+        };
     }
 
 }
