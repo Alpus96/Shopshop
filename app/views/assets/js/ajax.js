bfb862790bf78209c881c6e29dc12de71cf500a7
Alexander Ljungberg Perme <alex.perme@gmail.com>
2017-05-30 14:10:40 +0200

    Removed unnecessary comments

--- app/views/assets/js/ajax.js
+++ app/views/assets/js/ajax.js
@@ -7,52 +7,21 @@ class Ajax {
     *                   data: 		{username: "", password: ""}, an object containing
     *                               the information to send to the server.
     *                   callback: 	(error, result) => {...}, a function to call when done.
-    *
-    *   TODO: Use XMLHttpRequest to post. ($.ajax() requires jQuery)
-    **/
+	**/
 	post (url, data, callback) {
 		const request = new XMLHttpRequest();
-		request.open('POST', url, true);	//  Pass the input url parameter.
+		request.open('POST', url, true);
 		request.responseType = 'json';
 		request.setRequestHeader('Content-Type', 'Application/JSON');
-		//	Handle when the request succeds.
 		request.onload = () => {
-			//  Returns the result as result through the callback function.
 			callback(null, request.response);
 		};
-		//	Handle error response on request.
 		request.onerror = () => {
-			//  Log and return the error through the callback.
 			console.log(request.status, ':', request.statusText);
 			callback(true, request.status);
 		};
-		request.send(JSON.stringify(data));	//  Pass the input data parameter converted to JSON.
+		request.send(JSON.stringify(data));
 	}
-
-    /*post (url, data, callback) {
-        //  Ajax json post request.
-        $.ajax({
-            url: url,   //  Pass the input url parameter.
-            type: "POST",
-            crossDomain: true,
-            data: '{"some":"json"}',
-           // data: data, //  Pass the input data parameter.
-            dataType: "json",
-            success: (result) => {
-                //  Returns the result as result through the callback function.
-                //  TODO: Add comments about what response could be.
-                callback(null, result);
-            },
-            error: (xhr, ajaxOptions, thrownError) => {
-                //  If there was an error log it.
-                console.error(xhr.status);
-                console.error(ajaxOptions);
-                console.error(thrownError);
-                //  Then return error true through the callback.
-                callback(true, null);
-            }
-        });
-    }*/
 
     /**
     *       @description 	A function to send get requests to the server.
@@ -64,24 +33,16 @@ class Ajax {
     **/
     get (url, callback) {
         const request = new XMLHttpRequest();
-
-
         request.open('GET', url, true);
-
-        request.open('get', url);
-        origin/master
-
-        request.open('GET', url, true);
-        66ad0d344c79034403c673fa14b2c0f982cbfba2
         request.responseType = 'json';
-        request.send();
         request.onload = () => {
             callback(false, request.response);
         };
         request.onerror = () => {
-            console.log('Get request to ' + url + ' failed.');
-            callback(true, null);
+            console.log(request.status, ':', request.statusText);
+            callback(true, request.status);
         };
+		request.send();
     }
 
 }
