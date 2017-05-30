f7c440f232390146cb86981cbc4f1a5a78784652
Alexander Ljungberg Perme <alex.perme@gmail.com>
2017-05-30 14:15:14 +0200

    Fixed indentation

--- app/views/assets/js/ajax.js
+++ app/views/assets/js/ajax.js
@@ -1,12 +1,12 @@
 class Ajax {
     /**
-    *   @description    A function to send ajax requests with json data to the server.
+    *   @desc   A function to send ajax requests with json data to the server.
     *
-    *   @param			url: 		'/example', a string containing the url where to send
-    *                               the post request.
-    *                   data: 		{username: "", password: ""}, an object containing
-    *                               the information to send to the server.
-    *                   callback: 	(error, result) => {...}, a function to call when done.
+    *   @param	url: 		'/example', a string containing the url where to send
+    *           			the post request.
+    *           data: 		{username: "", password: ""}, an object containing
+    *           			the information to send to the server.
+    *           callback:	(error, result) => {...}, a function to call when done.
 	**/
 	post (url, data, callback) {
 		const request = new XMLHttpRequest();
@@ -24,12 +24,12 @@ class Ajax {
 	}
 
     /**
-    *       @description 	A function to send get requests to the server.
+    *	@desc 	A function to send get requests to the server.
     *
-    *       @param 			url: 		'/example', a string containing the relative url for
-    *                                    where to send the requets.
-    *						callback: 	(error, response) => {}, a function hadling the
-    *                                   response or error from the request.
+    *	@param 	url: 		'/example', a string containing the relative url for
+    *                       where to send the requets.
+    *			callback:	(error, response) => {}, a function hadling the
+    *                       response or error from the request.
     **/
     get (url, callback) {
         const request = new XMLHttpRequest();
